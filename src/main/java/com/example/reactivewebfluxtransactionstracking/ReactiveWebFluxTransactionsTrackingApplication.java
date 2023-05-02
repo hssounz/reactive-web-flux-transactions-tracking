a04d169feb71659205e72c7aaeadb835f0c523c3
Hassen <hassenbensaad55@gmail.com>
2023-05-03 00:06:32 +0200

    Intial commit

---
+++ src/main/java/com/example/reactivewebfluxtransactionstracking/ReactiveWebFluxTransactionsTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivewebfluxtransactionstracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebFluxTransactionsTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebFluxTransactionsTrackingApplication.class, args);
+	}
+
+}
